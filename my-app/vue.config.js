20a7285e6657eca921389418f726c1d90ab56f8f
Rafael Martorano <56660425+Warzinhuw@users.noreply.github.com>
2022-06-08 09:22:56 -0300

    Update vue.config.js

--- my-app/vue.config.js
+++ my-app/vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     devServer: {
+      disableHostCheck: true,
       proxy: {
         '^/api': {
           target: 'http://node-api:3080',
-          changeOrigin: true,
-          disableHostCheck: true
+          changeOrigin: true
         },
       }
     }
