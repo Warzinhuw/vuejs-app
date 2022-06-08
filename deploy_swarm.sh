56736b20f5ec529c23d05f29e1dc22bda879c1b3
Warzinhuw <rafael_martorano@hotmail.com>
2022-06-08 11:28:31 -0300

    Update lembrete

--- deploy_swarm.sh
+++ deploy_swarm.sh
@@ -2,4 +2,4 @@
 
 docker stack deploy --compose-file docker-compose.yml stackdemo
 
-echo "LEMBRETE, rodar depois docker swarm join-token manager para lembrar o token e docker service scale stackdemo_vue-ui=2 para escalar o front"
+echo "LEMBRETE, rodar depois docker swarm join-token worker para lembrar o token e docker service scale stackdemo_vue-ui=2 para escalar o front"
