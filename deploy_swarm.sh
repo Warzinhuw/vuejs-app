fccc2d35e141d170e9864e7bcaf852155bd714d9
Warzinhuw <rafael_martorano@hotmail.com>
2022-06-08 11:26:47 -0300

    Add lembretes

--- deploy_swarm.sh
+++ deploy_swarm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker stack deploy --compose-file docker-compose.yml stackdemo
+
+echo "LEMBRETE, rodar depois docker swarm join-token manager para lembrar o token e docker service scale stackdemo_vue-ui=2 para escalar o front"
